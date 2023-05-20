442305b2866480e3ab3e49d77720501ab9889cf3
softRahul10 <softrahul10@gmail.com>
2023-05-20 17:15:32 +0530

    modify app structure

--- server.js
+++ server.js
@@ -4,8 +4,15 @@ const path = require('path');
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set('view engine', 'ejs');
 app.set('views',path.join(__dirname, 'views'));
+
+app.get('/',(req, res) => {
+    res.render('index');
+})
 
 app.listen(3000, () => {
     console.log('Express server listening');
