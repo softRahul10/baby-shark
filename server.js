c2db64f9a26955bdbb13d39a2202683a896200aa
softRahul10 <softrahul10@gmail.com>
2023-05-20 19:22:17 +0530

    Project Done

--- server.js
+++ server.js
@@ -3,15 +3,23 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+const userslist = [];
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
-app.set('views',path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'));
 
-app.get('/',(req, res) => {
-    res.render('index');
+app.get('/', (req, res) => {
+    res.render('index', { pageName: 'Home page' });
+})
+app.get('/users', (req, res) => {
+    res.render('user', { pageName: 'Users', users: userslist });
+})
+app.post('/users', (req, res) => {
+    userslist.push(req.body.username);
+    res.redirect('/users');
 })
 
 app.listen(3000, () => {
