8c4fec6c403bee565cae5e62469444d20ba277bb
softRahul10 <softrahul10@gmail.com>
2023-05-20 16:56:11 +0530

    Register Templating engine

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const path = require('path');
 
 const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views',path.join(__dirname, 'views'));
 
 app.listen(3000, () => {
     console.log('Express server listening');
